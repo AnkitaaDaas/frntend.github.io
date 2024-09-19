80615fcd8b0c2122c1c87df08caa58179d490e61
Ankita Das <133870896+AnkitaaDaas@users.noreply.github.com>
2024-09-19 22:38:10 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css'; // Import CSS if not imported in App.js
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
